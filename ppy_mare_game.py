d7d95a752ec7dfb5a525cd316d3afed9a1d5c41c
unknown <s31302@pjwstk.edu.pl>
2025-05-19 00:22:59 +0200

    text typer and sound effects test

--- ppy_mare_game.py
+++ ppy_mare_game.py
@@ -1,7 +1,16 @@
 import tkinter as tk
 from tkinter import *
+import pygame
 class PpyMareGame:
     def __init__(self, root):
+        pygame.mixer.init()
+        pygame.mixer.set_num_channels(2)
+        self.chan_bg = pygame.mixer.Channel(0)
+        self.chan_type = pygame.mixer.Channel(1)
+
+        self.sound_bg = pygame.mixer.Sound('C418 - Wet Hands - Minecraft Volume Alpha.mp3')
+        self.sound_type = pygame.mixer.Sound('typing-sound-effect.mp3')  # podaj ścieżkę do muzyki
+
         self.frames = None
         self.root = root
         self.root.geometry("1400x700")
@@ -9,8 +18,11 @@ class PpyMareGame:
         self.after_id = None
         self.img_skip = PhotoImage(file='images/skip2.png')
 
+        self.chan_bg.play(self.sound_bg, loops=-1)
+        self.chan_bg.set_volume(0.3)
+
         self.frames_maker()
-        self.show_temp_frame('frameStart', 'frameMap', 10000)
+        self.show_temp_frame('frameStart', 'frameMap')
 
     def frames_maker(self):
         container = tk.Frame(self.root)
@@ -25,16 +37,51 @@ class PpyMareGame:
             f.grid(row=0, column=0, sticky="nsew")
             self.frames[frame] = f
 
-        tk.Button(self.frames['frameMap'], image=self.img_skip, background="black", borderwidth=0, activebackground="black", command=lambda: self.show_temp_frame('frameGame1', 'frameMap', 10000)).place(relx=0.17, rely=0.5, anchor="center")
-        tk.Button(self.frames['frameMap'], text="Game 2", command=lambda: self.show_temp_frame('frameGame2', 'frameMap', 10000)).place(relx=0.33, rely=0.25, anchor="center")
-        tk.Button(self.frames['frameMap'], text="Game 3", command=lambda: self.show_temp_frame('frameGame3', 'frameMap', 10000)).place(relx=0.50, rely=0.25, anchor="center")
+        tk.Button(self.frames['frameMap'], image=self.img_skip, background="black", borderwidth=0, activebackground="black", command=lambda: self.show_temp_frame('frameGame1', 'frameMap')).place(relx=0.17, rely=0.5, anchor="center")
+        tk.Button(self.frames['frameMap'], text="Game 2", command=lambda: self.show_temp_frame('frameGame2', 'frameMap')).place(relx=0.33, rely=0.25, anchor="center")
+        tk.Button(self.frames['frameMap'], text="Game 3", command=lambda: self.show_temp_frame('frameGame3', 'frameMap')).place(relx=0.50, rely=0.25, anchor="center")
 
-        tk.Button(self.frames['frameMap'], text="Game 6", command=lambda: self.show_temp_frame('frameGame6', 'frameMap', 10000)).place(relx=0.67, rely=0.5, anchor="center")
-        tk.Button(self.frames['frameMap'], text="Game 7", command=lambda: self.show_temp_frame('frameGame7', 'frameMap', 10000)).place(relx=0.83, rely=0.5, anchor="center")
+        tk.Button(self.frames['frameMap'], text="Game 6", command=lambda: self.show_temp_frame('frameGame6', 'frameMap')).place(relx=0.67, rely=0.5, anchor="center")
+        tk.Button(self.frames['frameMap'], text="Game 7", command=lambda: self.show_temp_frame('frameGame7', 'frameMap')).place(relx=0.83, rely=0.5, anchor="center")
         # przekazuje wlsciwosci z game 2 i 3 do 4 i 5
-        tk.Button(self.frames['frameMap'], text="Game 4", command=lambda: self.show_temp_frame('frameGame2', 'frameMap', 10000)).place(relx=0.33, rely=0.75, anchor="center")
-        tk.Button(self.frames['frameMap'], text="Game 5", command=lambda: self.show_temp_frame('frameGame3', 'frameMap', 10000)).place(relx=0.50, rely=0.75, anchor="center")
+        tk.Button(self.frames['frameMap'], text="Game 4", command=lambda: self.show_temp_frame('frameGame2', 'frameMap')).place(relx=0.33, rely=0.75, anchor="center")
+        tk.Button(self.frames['frameMap'], text="Game 5", command=lambda: self.show_temp_frame('frameGame3', 'frameMap')).place(relx=0.50, rely=0.75, anchor="center")
 
+        self.story_texts = {
+            'frameStart': ("Budzi cię deszcz(mokro na twarzy). Rozglądasz się dookoła, wszystko wygląda jak ze snu. "
+                           "Wygląda na to, że jesteś w środku lasu, leżysz na mchu, ale nie pamiętasz skąd się tu wzięłaś. "
+                           "Coś przykuło twoją uwagę, coś miga w oddali.Podchodzisz bliżej, okazuje się, że pochodzi to z jaskinii. "
+                           "Zaciekawiona wchodzisz."),
+            'frameGame1': ("Na wejściu widzisz postać. Przypomina (wklej opis postaci). Może ona ci powie coś więcej? "
+                           "Zaintrygowana podchodzisz bliżej "),
+            'frameGame2': ("Upadasz na ziemię i to chyba cud, że nic nie jest.\n Rozglądasz się okazuje się że jesteś na małej wyspie.\n "
+                           "Jest bardzo przyjemnie.\n sratatata świeci słońce.\n okazuje się że spadłaś na bloba, dlatego nic ci nie jest. "),
+            'frameGame3': ("Jesteś w szkole, znowu czujesz się jakbyś była dzieckiem.\n Zalewają cię wspomnienia,\n które niekoniecznie są pozytywne."
+                           "\n Dawny stres wraca i czujesz pot na rękach.\n Przed tobą pojawia się ta jedna dziewczyna,\n która zawsze się z ciebie śmiała.\n "
+                           "Znowu czujesz jej oceniający wzrok na sobie,\n jej głośnę żucie gumy doprowadza się do szału."),
+            'frameGame6': ("Idziesz do łazienki przemyć twarz. Podnosisz wzrok na lustro, ale w lustrze to nie jest twoja twarz .o. "
+                          "Postać z lustra zaczyna wychodzić i wiesz, że nie ma dobrych zamirów (intuicja). Chwytasz za klamkę i wybiegasz z łazienki prosto na hotelowy korytarz. "
+                          "Biegniesz przed siebie dopóki nie stracisz tchu oglądasz się za siebie spodziewając się TEGO CZEGOŚ, ale korytarz jest pusty. Ufff najwidoczniej udało ci się uciec. "
+                          "Znowu patrzysz przed siebie i chcesz ruszyć dalej by znaleźć wyjście, ale przed tobą stoi pies. (opis psa). \"pachniesz ośćmi\" odzywa się pies (czym cię zaskakuje). "
+                          "\"podziel się, jestem  G Ł O D N Y\""),
+            'frameGame7': ("Wtedy podnosisz wzrok i na końcu korytarza widzisz swoją utraconą miłość. Tak to ta osoba, ta z którą spędziłęś swoje cudowne wakacje. "
+                          "Co ona tu robi? Czujesz dezorientację i w przypływie emocji zaczynasz rozpaczliwie biec w jej kierunku, krzycząc jej imię, ale ona znika za drzwiami. "
+                          "Dobiegasz do nich i chwodzisz do pokoju za nimi(mapa vol2) a tam jest nicość, jedynie wielka ryba z zębami i nogami :). "
+                          "\"Obserwowałem cię przez cały czas, to ja, jestem tym głosem, który opisuje twoją rzeczywistość, twoja dezorientacja mnie cieszy. "
+                          "Nie zamierzam cię stąd wypuścić.")
+        }
+
+        self.story_labels = {}
+        for key in self.story_texts.keys():
+            lbl = tk.Label(self.frames[key], text="", fg="white", bg="black", font=("Helvetica", 30), wraplength=1000)
+            lbl.place(x=0, y=0, relwidth=1, relheight=1)
+            self.story_labels[key] = lbl
+
+            # Przycisk skip dla każdej ramki
+            tk.Button(self.frames[key], image=self.img_skip, borderwidth=0, background='black',
+                      activebackground='black',
+                      command=lambda k=key: self.show_frame('frameMap')).place(relx=0.87, rely=0.80)
+        '''
         # frameStart view
         tk.Label(self.frames['frameStart'],text="Budzi cię deszcz(mokro na twarzy). Rozglądasz się dookoła, wszystko wygląda jak ze snu. "
                       "Wygląda na to, że jesteś w środku lasu, leżysz na mchu, ale nie pamiętasz skąd się tu wzięłaś. "
@@ -48,6 +95,15 @@ class PpyMareGame:
                                           "Zaintrygowana podchodzisz bliżej ", fg="white", bg="black",
                                      font=("Helvetica", 30), wraplength=1000).place(x=0, y=0, relwidth=1, relheight=1)
         tk.Button(self.frames['frameGame1'], image=self.img_skip, borderwidth=0, background='black', activebackground='black', command=lambda: self.show_frame('frameMap')).place(relx=0.87, rely=0.80)
+
+        #wypsiywanie proba
+        labelHistoryGame1 = tk.Label(self.frames['frameGame1'], text="", fg="white", bg="black", font=("Helvetica", 30), wraplength=1000)
+        labelHistoryGame1.place(x=0, y=0, relwidth=1, relheight=1)
+        tekst = ("Na wejściu widzisz postać. Przypomina (wklej opis postaci). Może ona ci powie coś więcej? Zaintrygowana podchodzisz bliżej ")
+        self.text_typer(labelHistoryGame1, tekst)
+
+        tk.Button(self.frames['frameGame1'], image=self.img_skip, borderwidth=0, background='black',
+                  activebackground='black', command=lambda: self.show_frame('frameMap')).place(relx=0.87, rely=0.80)
 
         # frameGame2 view
         tk.Label(self.frames['frameGame2'],text="Upadasz na ziemię i to chyba cud, że nic nie jest.\n Rozglądasz się okazuje się że jesteś na małej wyspie.\n "
@@ -80,6 +136,21 @@ class PpyMareGame:
                                           "Nie zamierzam cię stąd wypuścić.", fg="white", bg="black",
                                      font=("Helvetica", 30), wraplength=1000).place(x=0, y=0, relwidth=1, relheight=1)
         tk.Button(self.frames['frameGame7'], image=self.img_skip, borderwidth=0, background='black', activebackground='black', command=lambda: self.show_frame('frameMap')).place(relx=0.87, rely=0.80)
+        '''
+    def text_typer(self, label, text, i=0, delay=75):
+        if i == 0:
+            # Start muzyki gdy zaczynamy pisać tekst
+            if not pygame.mixer.music.get_busy():
+                pygame.mixer.music.play(-1, fade_ms=2000)
+
+        if (i < len(text)):
+            label.config(text=label["text"] + text[i])
+            label.after(delay, self.text_typer, label, text, i + 1, delay)
+
+        else:
+            # Tekst napisany, zatrzymaj muzykę tutakj zmienic jak typic adnhfrnciuvcHELSP
+            #pygame.mixer.music.stop()
+            pygame.mixer.music.fadeout(2000)
 
     def show_frame(self, name):
         if self.after_id:
@@ -87,9 +158,28 @@ class PpyMareGame:
             self.after_id = None
         self.frames[name].tkraise()
 
-    def show_temp_frame(self, temp_frame, return_frame, delay_ms):
+        # zatrzymaj muzykę jeśli przechodzisz z ramki z historią na inną ramkę
+        if pygame.mixer.music.get_busy():
+            pygame.mixer.music.fadeout(1000)
+
+        if name in self.story_texts:
+            lbl = self.story_labels[name]
+            lbl.config(text="")  # czyścimy tekst przed pisaniem
+            self.text_typer(lbl, self.story_texts[name])
+
+    def calculate_display_time(self, text, delay=80, read_time=10):
+        time_to_type = (len(text) * delay) / 1000  # zamiana ms na s
+        return int((time_to_type + read_time) * 1000)  # zwracamy czas w ms dla after()
+
+    def show_temp_frame(self, temp_frame, return_frame):
         self.show_frame(temp_frame)
-        self.after_id = self.root.after(delay_ms, lambda: self.show_frame(return_frame))
+        text = self.story_texts.get(temp_frame, "")
+        delay = 80  # ms delay dla efektu pisania
+        total_time = self.calculate_display_time(text, delay=delay, read_time=10)  # 10 sekund na przeczytanie
+        if self.after_id:
+            self.root.after_cancel(self.after_id)
+        self.after_id = self.root.after(total_time, lambda: self.show_frame(return_frame))
+
 
 if __name__ == "__main__":
     root = tk.Tk()
